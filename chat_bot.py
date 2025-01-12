3e552fc389705dfecff8255f59a413c59111b3ec
nvspranathi <19h71a0453@gmail.com>
2025-01-12 18:17:26 +0530

    adding color to the text on main screen, little bit of styling added

--- chat_bot.py
+++ chat_bot.py
@@ -5,7 +5,7 @@ from tkinter import scrolledtext,messagebox
 def OnClick_Submit():
     UserInput = user_space.get("1.0",tk.END)
     bot_screen.configure(state=tk.NORMAL)
-    bot_screen.insert(tk.END,"\nUser: "+UserInput)
+    bot_screen.insert(tk.END,"\nUser: "+UserInput,"user")
     user_space.delete("1.0",tk.END)
     bot_screen.configure(state=tk.DISABLED)
 
@@ -13,20 +13,21 @@ def OnClick_Submit():
 root = tk.Tk()
 
 # heading for the window or the interface
-app_header = tk.Label(root,text="MY CHAT BOT",font=("Georgia",20))
+app_header = tk.Label(root,text="MY CHAT BOT",font=("Georgia",20),bg="BLACK",fg="WHITE")
 app_header.pack(fill=tk.BOTH,expand=True)
 
 # scrolled text for the bot
 bot_screen = scrolledtext.ScrolledText(root,wrap=tk.WORD,bg="WHITE",state=tk.DISABLED,height=30)
 bot_screen.pack(fill=tk.BOTH,expand=True)
+bot_screen.tag_config("user",foreground="BLUE",font=("Georgia",12))
 
 # scrolled text for the user
-user_space = scrolledtext.ScrolledText(root,wrap=tk.WORD,bg="LIGHT GREY",height=2)
-user_space.pack(fill=tk.BOTH,expand=True)
+user_space = scrolledtext.ScrolledText(root,wrap=tk.WORD,bg="LIGHT GREY",height=2,width=80)
+user_space.pack()
 
 # submit button
 submit_button = tk.Button(root,text="Submit",height=2,width=10,command=OnClick_Submit)
-submit_button.pack(anchor=tk.W,padx=10)
+submit_button.pack(anchor=tk.CENTER,padx=10)
 
 # main loop
 root.mainloop()
